795f7cd140a838c62d5e15352abb80ba78a1efc2
KodywiththeK <jmsds.km@gmail.com>
2023-07-25 19:40:20 +0900

    env set-up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   transpilePackages: ['antd'],
 }
 
 module.exports = nextConfig
+module.exports = {
+  transpilePackages: ['antd'],
+  env: {
+    API_BASE_URL: process.env.MAIN_API_URL === 'production' ? 'https://jgt0ls7201.execute-api.ap-northeast-2.amazonaws.com/prod/camps' : 'https://jgt0ls7201.execute-api.ap-northeast-2.amazonaws.com/dev/camps',
+    // Add other environment variables as needed
+  },
+}
