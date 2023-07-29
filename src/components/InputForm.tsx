a68bfc49aad6716fb7c487ab72dcf2ab8b3b0ab2
KodywiththeK <jmsds.km@gmail.com>
2023-07-30 04:30:50 +0900

    반응형 수정

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -95,40 +95,42 @@ export default function InputForm({ content, id }: Props) {
           <label className="label w-auto sm:w-32 sm:flex sm:justify-end">
             <span className="label-text">교육과정 이름</span>
           </label>
-          <input type="text" placeholder="Type here" value={info.title} onChange={(e) => setInfo({ ...info, title: e.target.value })} required className="input w-full max-w-xl input-sm md:input-md" />
+          <input type="text" placeholder="Type here" value={info.title} onChange={(e) => setInfo({ ...info, title: e.target.value })} required className="input sm:grow w-full sm:w-auto max-w-xl input-sm md:input-md" />
         </div>
         <div className="w-full sm:flex sm:flex-row gap-2 flex-col sm:items-start">
           <label className="label w-auto sm:w-32 sm:flex sm:justify-end">
             <span className="label-text">상세설명</span>
           </label>
-          <textarea placeholder="Type here" value={info.description} onChange={(e) => setInfo({ ...info, description: e.target.value })} required className="textarea w-full max-w-xl textarea-sm md:textarea-md h-32 " />
+          <textarea placeholder="Type here" value={info.description} onChange={(e) => setInfo({ ...info, description: e.target.value })} required className="textarea grow w-full sm:w-auto max-w-xl textarea-sm md:textarea-md h-32 " />
         </div>
         <div className="w-full sm:flex gap-2 items-center ">
           <label className="label w-auto sm:w-32 sm:flex sm:justify-end">
             <span className="label-text">교육기관 이름</span>
           </label>
-          <input type="text" value={info.company} onChange={(e) => setInfo({ ...info, company: e.target.value })} required placeholder="Type here" className="input w-full max-w-xl input-sm md:input-md" />
+          <input type="text" value={info.company} onChange={(e) => setInfo({ ...info, company: e.target.value })} required placeholder="Type here" className="input grow w-full sm:w-auto max-w-xl input-sm md:input-md" />
         </div>
-        <div className="w-full sm:flex gap-2 items-center ">
+        <div className="w-full sm:flex-row flex flex-col gap-2 sm:items-center  ">
           <label className="label w-auto sm:w-32 sm:flex sm:justify-end">
             <span className="label-text">교육과정 키워드</span>
           </label>
-          {info.stack.length > 0 && (
-            <ul className="flex items-center gap-2 py-2 flex-wrap">
-              {info.stack.map((item, idx) => (
-                <li key={idx} onClick={(e) => handleRemoveKeyword(e, idx)} className=" cursor-pointer bg-green-100 shadow rounded-lg p-2">
-                  {item}
-                </li>
-              ))}
-            </ul>
-          )}
-          <input type="text" placeholder="띄어쓰기로 구분해주세요" onKeyDown={(e) => handleSetKeyword(e)} value={keywordInput} onChange={(e) => setKeywordInput(e.target.value)} className="input w-full max-w-xl input-sm md:input-md" />
+          <div className="grow">
+            {info.stack.length > 0 && (
+              <ul className="flex items-center gap-2 py-2 flex-wrap">
+                {info.stack.map((item, idx) => (
+                  <li key={idx} onClick={(e) => handleRemoveKeyword(e, idx)} className=" cursor-pointer bg-green-100 shadow rounded-lg p-2">
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            )}
+            <input type="text" placeholder="띄어쓰기로 구분해주세요" onKeyDown={(e) => handleSetKeyword(e)} value={keywordInput} onChange={(e) => setKeywordInput(e.target.value)} className="input w-full max-w-xl input-sm md:input-md" />
+          </div>
         </div>
         <div className="w-full sm:flex gap-2 items-center ">
           <label className="label w-auto sm:w-32 sm:flex sm:justify-end">
             <span className="label-text">이벤트</span>
           </label>
-          <select value={info.event} onChange={(e) => setInfo({ ...info, event: e.target.value as EventType })} className="select select-sm md:select-md w-full max-w-xl">
+          <select value={info.event} onChange={(e) => setInfo({ ...info, event: e.target.value as EventType })} className="select select-sm md:select-md grow w-full sm:w-auto max-w-xl">
             <option disabled value="">
               이벤트를 선택해주세요
             </option>
